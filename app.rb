ee8a479aaa37c5e9fca7e8f3bf229309e1d0aab7
Brian Fan <kuomu.fan@yahoo.com>
2017-04-28 11:14:51 -0700

    add form to add definition to the word and add different routes to display definitions for each word

--- app.rb
+++ app.rb
@@ -10,14 +10,35 @@ get('/') do
   erb(:index)
 end
 
-post('/word_form') do
-  word = params.fetch('word')
-  test_word = Word.new(:text=> word)
+post('/add_word') do
+  user_input = params.fetch('word')
+  test_word = Word.new(:text=> user_input)
   test_word.save()
   @@words = Word.all()
   erb(:index)
 end
 
-get('/words/:id')
+get('/words/:id') do
+  word_id = params.fetch('id').to_i()
+  @word = Word.find(word_id)
+  @definitions = @word.definitions()
+  erb(:word)
+end
+
+
+##Each word add definitions
+get('/words/:id/add_definition') do
+  word_id = params.fetch('id').to_i()
+  @word = Word.find(word_id)
+  erb(:definition_form)
+end
+
+
+post('/add_definition') do
+  user_input = params.fetch('phrase')
+  @new_phrase = Definition.new(:phrase => user_input)
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@new_phrase)
+  @definitions = @word.definitions()
   erb(:word)
 end
