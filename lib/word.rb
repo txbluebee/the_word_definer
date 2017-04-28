9bac82b415425af449fa2fffede3a9b1cfc1a800
Brian Fan <kuomu.fan@yahoo.com>
2017-04-28 09:05:44 -0700

    add .clear, #save, #id methods for Word Class

--- lib/word.rb
+++ lib/word.rb
@@ -4,16 +4,32 @@ class Word
 
   def initialize(attributes)
     @text = attributes.fetch(:text)
+    @id = @@words.length() + 1
     @definitions = []
   end
 
+  def id
+    @id
+  end
+    
   def self.all
     @@words
   end
 
+  def self.clear
+    @@words = []
+  end
+
+  def save
+    @@words.push(self)
+  end
+
+
+
   def definitions
     @definitions
   end
+
 
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,6 +4,10 @@ require('rspec')
 
 describe(Word) do
 
+  before() do
+    Word.clear()
+  end
+
   describe('#text') do
     it('returns the text of the word') do
       test_word = Word.new(:text =>'hello')
@@ -17,6 +21,20 @@ describe(Word) do
     end
   end
 
+  describe(".clear") do
+    it('clears words in the array @@words') do
+      expect(Word.clear()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the word to the words array') do
+      test_word = Word.new(:text =>'hello')
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
   describe('#definitions') do
     it('returns the array of difinitions for the word') do
       test_word = Word.new(:text =>'hello')
@@ -24,5 +42,14 @@ describe(Word) do
     end
   end
 
+  describe('#id') do
+    it('returns the id number assigned to the word when created') do
+      word_1 = Word.new(:text =>'hello')
+      word_1.save()
+      word_2 = Word.new(:text =>'world')
+      word_2.save()
+      expect(word_2.id()).to(eq(2))
+    end
 
+  end
 end
